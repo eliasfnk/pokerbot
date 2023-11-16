662e37d02c1db91bda6aa34e20ec692c5f5314c8
Oliver Tomaskovic <oltoit00@hs-esslingen.de>
2023-11-16 20:33:29 +0100

    get cards

--- src/main/java/org/continuouspoker/player/logic/Risk.java
+++ src/main/java/org/continuouspoker/player/logic/Risk.java
@@ -1,9 +1,17 @@
 package org.continuouspoker.player.logic;
 
+import java.util.List;
+
+import org.continuouspoker.player.model.Player;
 import org.continuouspoker.player.model.Table;
 
 public class Risk {
-    public Risk(Table table){
 
+
+
+    public Risk(Table table){
+        List<Player> players = table.getPlayers();
+        Player player = players.get(table.getActivePlayer());
+        player.getCards();
     }
 }
