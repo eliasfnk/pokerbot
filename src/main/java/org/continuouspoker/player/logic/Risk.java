3457d61da02d3d6a9d49481ed6b7862c57fa9d7f
Oliver Tomaskovic <oltoit00@hs-esslingen.de>
2023-11-16 20:36:05 +0100

    cards als liste ausgegeben

--- src/main/java/org/continuouspoker/player/logic/Risk.java
+++ src/main/java/org/continuouspoker/player/logic/Risk.java
@@ -2,6 +2,7 @@ package org.continuouspoker.player.logic;
 
 import java.util.List;
 
+import org.continuouspoker.player.model.Card;
 import org.continuouspoker.player.model.Player;
 import org.continuouspoker.player.model.Table;
 
@@ -12,6 +13,6 @@ public class Risk {
     public Risk(Table table){
         List<Player> players = table.getPlayers();
         Player player = players.get(table.getActivePlayer());
-        player.getCards();
+        List<Card> cards = player.getCards();
     }
 }
