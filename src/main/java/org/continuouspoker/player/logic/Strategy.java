06ab0e134b7cbb99e611dcd4a9109b7b7c9adf74
Oliver Tomaskovic <oltoit00@hs-esslingen.de>
2023-11-16 21:41:35 +0100

    whoooooooo random

--- src/main/java/org/continuouspoker/player/logic/Strategy.java
+++ src/main/java/org/continuouspoker/player/logic/Strategy.java
@@ -1,18 +1,25 @@
 package org.continuouspoker.player.logic;
 
+import java.util.Random;
+
 import org.continuouspoker.player.model.Bet;
 import org.continuouspoker.player.model.Table;
 
 public class Strategy {
 
       int bet;
+      Random rnd = new Random(100);
 
    public Bet decide(final Table table) {
       System.out.println(table);
-      Evaluation evaluation = new Evaluation(table);
-
-      //wenn cardvalue unter minimum bet ist ist bet = minimum bet
-      bet = evaluation.cardValue < table.getMinimumBet()? table.getMinimumBet() : evaluation.cardValue;
+      try{
+            Evaluation evaluation = new Evaluation(table);
+      
+            //wenn cardvalue unter minimum bet ist ist bet = minimum bet
+            bet = evaluation.cardValue < table.getMinimumBet()? table.getMinimumBet() : evaluation.cardValue;
+      }catch(Exception e){
+            bet = rnd.nextInt();
+      }
 
       return new Bet().bet(bet);
    }
