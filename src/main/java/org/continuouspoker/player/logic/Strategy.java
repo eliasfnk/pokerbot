0aff3c80fb0e7777d0240204a7b8016365c4b81b
Oliver Tomaskovic <oltoit00@hs-esslingen.de>
2023-11-16 21:20:29 +0100

    evaluation jaja

--- src/main/java/org/continuouspoker/player/logic/Strategy.java
+++ src/main/java/org/continuouspoker/player/logic/Strategy.java
@@ -5,10 +5,16 @@ import org.continuouspoker.player.model.Table;
 
 public class Strategy {
 
+      int bet;
+
    public Bet decide(final Table table) {
       System.out.println(table);
       Evaluation evaluation = new Evaluation(table);
-      return new Bet().bet(evaluation.cardValue);
+
+      //wenn cardvalue unter minimum bet ist ist bet = minimum bet
+      bet = evaluation.cardValue < table.getMinimumBet()? table.getMinimumBet() : evaluation.cardValue;
+
+      return new Bet().bet(bet);
    }
 
 }
